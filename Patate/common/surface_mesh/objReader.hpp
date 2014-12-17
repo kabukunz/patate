64aef8d6a0fa999fae125c20705ced10d133e5c4
Simon Boyé <sim.boye@gmail.com>
2014-12-17 17:34:42 +0100

    Vitelotte: Fixed an infinite loop in OBJReader.

--- Patate/common/surface_mesh/objReader.hpp
+++ Patate/common/surface_mesh/objReader.hpp
@@ -21,11 +21,11 @@ OBJBaseReader::read(std::istream& in)
     while(in.good())
     {
         // comment
-        if(m_line.empty() || m_line[0] == '#' || std::isspace(m_line[0]))
-            continue;
-
-        m_lineStream >> spec;
-        parseDefinition(spec, m_lineStream);
+        if(!m_line.empty() && m_line[0] != '#' && !std::isspace(m_line[0]))
+        {
+            m_lineStream >> spec;
+            parseDefinition(spec, m_lineStream);
+        }
 
         readLine(in);
     }
