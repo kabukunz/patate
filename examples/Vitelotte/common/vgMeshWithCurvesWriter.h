ad933369c9f4599617a2e42634a756b8733e8c23
Simon Boyé <sim.boye@gmail.com>
2015-05-04 15:45:58 +0200

    Added missing license header in various files.

--- examples/Vitelotte/common/vgMeshWithCurvesWriter.h
+++ examples/Vitelotte/common/vgMeshWithCurvesWriter.h
@@ -1,3 +1,9 @@
+/*
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #ifndef _EXAMPLES_VITELOTTE_COMMON_VG_MESH_WITH_CURVES_WRITER_
 #define _EXAMPLES_VITELOTTE_COMMON_VG_MESH_WITH_CURVES_WRITER_
 
--- examples/Vitelotte/mvgtk/plotCommand.h
+++ examples/Vitelotte/mvgtk/plotCommand.h
@@ -1,3 +1,9 @@
+/*
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #ifndef _MVGTK_PLOT_COMMAND_
 #define _MVGTK_PLOT_COMMAND_
 
